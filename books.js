084a7450c17704ec70b1fbf7d1bedfba2ef4ffba
matthewkollar <mattkollar15@gmail.com>
2021-10-17 14:45:26 -0700

    First commit

---
+++ books.js
@@ -0,0 +1,26 @@
+function addFavoriteBook(bookName) {
+    if (!bookName.includes("Great")) {
+      favoriteBooks.push(bookName);
+      }
+  }
+  
+  function printFavoriteBooks() {
+    console.log(
+      `Favorite Books: ${favoriteBooks.length}`
+    );
+      for (let bookName of favoriteBooks) {
+        console.log(bookName);
+      }
+  }
+    
+    
+    let favoriteBooks = [];
+    
+    addFavoriteBook("A Song of Ice and Fire");
+    addFavoriteBook("The Great Gatsby");
+    addFavoriteBook("Crime & Punishment");
+    addFavoriteBook("Great Expectations");
+    addFavoriteBook("You Don't Know JS");
+  
+  printFavoriteBooks();
+    
